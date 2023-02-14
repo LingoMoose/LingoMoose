2937d0571edefd3ccd538028d99fe5fc46eb9280
Aaron <aaron1venema@gmail.com>
2023-02-14 17:31:56 -0600

    faq

---
+++ src/data/FAQ.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+      id: 1,
+      title: 'What is this website all about?',
+      info:
+        'This website is a free, open-source platform for learning the Vietnamese language. Users can contribute to creating stories to help with the learning process.',
+    },
+    {
+      id: 2,
+      title: 'Is this website free to use?',
+      info:
+        'Yes, this website is completely free to use.',
+    },
+    {
+      id: 3,
+      title: 'What is the target audience for this website?',
+      info:
+        'This website is aimed at individuals who are interested in learning the Vietnamese language.',
+    },
+    {
+      id: 4,
+      title: 'How can I contribute to the website?',
+      info:
+        'No, you don\'t need to have prior knowledge of Vietnamese. However, it would be helpful if you have some basic knowledge of the language to ensure the stories you create are accurate and helpful.',
+    },
+    {
+      id: 5,
+      title: 'Are there any rules for contributing to the website?',
+      info:
+        'Yes, there are some rules for contributing to the website. All content must be appropriate and in line with the website\'s mission to provide a safe and supportive environment for language learning.',
+    },
+  ]
+  export default questions
+
