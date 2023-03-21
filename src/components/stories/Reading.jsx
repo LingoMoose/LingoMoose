03c97ccfc0fb34cfe1ed8afb7abcaad4273aa46a
Aaron <aaron1venema@gmail.com>
2023-03-20 20:10:21 -0500

    regex

--- src/components/stories/Reading.jsx
+++ src/components/stories/Reading.jsx
@@ -79,8 +79,10 @@ const Reader = ({ text, audioUrl, translation }) => {
   },[fontSize])
 
   const handleSentenceClick = (sentence) => {
-    const sentenceIndex = text.split(/(.*?[.?!]['"’”]?)(?=\s+(?:[A-ZĐI])|\s*$)/gm).indexOf(sentence);
-    const selectedSentenceTranslation = translation.split(/(.*?[.?!]['"’”]?)(?=\s+(?:[A-ZĐI])|\s*$)/gm)[sentenceIndex];
+    const sentenceIndex = text.split(/(.*?[.?!]['"’”]?)(?=\s+(?:[A-ZĐIÁÀẢẠÂẤẦẨẬẪĂẮẰẲẴẶÉÈẺẸÊẾỀỂỄỆÍÌỈỊÓÒỎỌÔỐỒỔỖỘƠỚỜỞỢỠÚÙỦỤƯỨỪỬỰỮYÝỲỶỴĐ]|[A-Z]|\s*$))/gm
+).indexOf(sentence);
+    const selectedSentenceTranslation = translation.split(/(.*?[.?!]['"’”]?)(?=\s+(?:[A-ZĐIÁÀẢẠÂẤẦẨẬẪĂẮẰẲẴẶÉÈẺẸÊẾỀỂỄỆÍÌỈỊÓÒỎỌÔỐỒỔỖỘƠỚỜỞỢỠÚÙỦỤƯỨỪỬỰỮYÝỲỶỴĐ]|[A-Z]|\s*$))/gm
+)[sentenceIndex];
     setSelectedTranslation(selectedSentenceTranslation);
   };
 
@@ -171,7 +173,8 @@ const Reader = ({ text, audioUrl, translation }) => {
         <div className='p-4 px-6 shadow-sm z-10'
         style={{ backgroundColor: 'var(--background-color4)'}}
         >
-        {text.split(/(.*?[.?!]['"’”]?)(?=\s+(?:[A-ZĐI])|\s*$)/gm).map((sentence, index) => (
+        {text.split(/(.*?[.?!]['"’”]?)(?=\s+(?:[A-ZĐIÁÀẢẠÂẤẦẨẬẪĂẮẰẲẴẶÉÈẺẸÊẾỀỂỄỆÍÌỈỊÓÒỎỌÔỐỒỔỖỘƠỚỜỞỢỠÚÙỦỤƯỨỪỬỰỮYÝỲỶỴĐ]|[A-Z]|\s*$))/gm
+).map((sentence, index) => (
             <p 
                 key={index}
                 className={`inline  leading-loose cursor-pointer hover:rounded-lg ${hoverEffect}
